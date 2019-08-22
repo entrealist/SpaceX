90d38709f6ba59814a524ebe5a200d67ad18714a
ritwikjamuar <ritwikjamuar@gmail.com>
2019-08-22 22:10:01 +0530

    Added LaunchesOptionsAdapter to serve as an adapter for ViewPager and TabLayout in LaunchesFragment. Thus, created : LaunchesOptionsAdapter : src -> ritwik.samples.spacex -> ui -> main -> fragments -> adapters

---
+++ app/src/main/java/ritwik/samples/spacex/ui/main/fragments/adapters/LaunchesOptionsAdapter.kt
@@ -0,0 +1,29 @@
+package ritwik.samples.spacex.ui.main.fragments.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class LaunchesOptionsAdapter ( supportFragmentManager : FragmentManager )
+	: FragmentStatePagerAdapter ( supportFragmentManager ) {
+	// Variables.
+	private val fragmentList = ArrayList < Fragment > ()
+	private val fragmentTitleList = ArrayList < String > ()
+
+	/*---------------------------- FragmentStatePagerAdapter Callbacks ---------------------------*/
+
+	override fun getItem ( position : Int ) : Fragment {
+		return fragmentList [ position ]
+	}
+
+	override fun getCount() : Int {
+		return fragmentList.size
+	}
+
+	/*-------------------------------------- Public Methods --------------------------------------*/
+
+	fun addFragment ( fragment : Fragment, title : String ) {
+		fragmentList.add ( fragment )
+		fragmentTitleList.add ( title )
+	}
+}
