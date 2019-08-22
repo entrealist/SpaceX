9e9ee1f49e3ee882e0482baeac7e8f9dfe68d9a5
ritwikjamuar <ritwikjamuar@gmail.com>
2019-08-23 03:25:42 +0530

    Added Google Material as dependency for implementing Navigation Drawer. Changed the Primary, Primary Dark and Accent color of project. Added strings related to menu items of Navigation Drawer. Added Navigation Drawer to MainActivity. Added Navigation Drawer Header Layout. Added Icons SVG to Drawables.
    Thus, following files were affected:
    1. MainActivity.kt
    2. activity_main.xml
    3. colors.xml
    4. strings.xml
    5. build.gradle
    
    Also, following files were created:
    1. ic_about_us.xml
    2. ic_company.xml
    3. ic_cores.xml
    4. ic_history.xml
    5. ic_launches.xml
    6. ic_vehicles.xml
    7. navigation_drawer_header_layout_main.xml
    8. main_menu.xml

--- app/src/main/java/ritwik/samples/spacex/ui/main/MainActivity.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/main/MainActivity.kt
@@ -2,9 +2,20 @@ package ritwik.samples.spacex.ui.main
 
 import android.os.Bundle
 
+import android.view.Menu
+import android.view.MenuItem
+
 import androidx.appcompat.app.AppCompatActivity
 
+import androidx.appcompat.widget.Toolbar
+
+import androidx.core.view.GravityCompat
+
+import androidx.drawerlayout.widget.DrawerLayout
+
 import androidx.fragment.app.FragmentManager
+
+import com.google.android.material.navigation.NavigationView
 
 import ritwik.samples.spacex.R
 
@@ -15,12 +26,95 @@ class MainActivity
 	: AppCompatActivity (),
 	LaunchesFragment.Listener,
 	LaunchesListFragment.Listener {
+	// Views.
+	private var toolbar : Toolbar? = null
+	private var drawerLayout : DrawerLayout? = null
+	private var navigationView : NavigationView? = null
+
+	/**[NavigationView.OnNavigationItemSelectedListener] for listening the user selection of
+	 * option in Navigation Drawer.*/
+	private val navigationItemSelectedListener = NavigationView.OnNavigationItemSelectedListener {
+		when ( it.itemId ) {
+			R.id.menu_item_launches -> {
+
+			}
+
+			R.id.menu_item_vehicle -> {
+
+			}
+
+			R.id.menu_item_core -> {
+
+			}
+
+			R.id.menu_item_company -> {
+
+			}
+
+			R.id.menu_item_history -> {
+
+			}
+
+			R.id.menu_item_about -> {
+
+			}
+		}
+		true
+	}
 
 	/*----------------------------------- Activity Callbacks -------------------------------------*/
 
 	override fun onCreate ( savedInstanceState : Bundle? ) {
 		super.onCreate ( savedInstanceState )
 		setContentView ( R.layout.activity_main )
+		initializeViews ()
+	}
+
+	override fun onCreateOptionsMenu ( menu : Menu? ) : Boolean {
+		menuInflater.inflate ( R.menu.main_menu, menu )
+		return super.onCreateOptionsMenu ( menu )
+	}
+
+	override fun onOptionsItemSelected ( item : MenuItem? ) : Boolean {
+		when ( item?.itemId ) {
+			android.R.id.home -> {
+				drawerLayout?.closeDrawer ( GravityCompat.START )
+			}
+		}
+		return true
+	}
+
+	/*------------------------------------- Private Methods --------------------------------------*/
+
+	/**Method to initialize and configure the views of MainActivity.*/
+	private fun initializeViews () {
+		initializeNavigationDrawer ()
+		initializeToolbar ()
+	}
+
+	/**Method to initialize and configure the Toolbar in MainActivity.*/
+	private fun initializeToolbar () {
+		// Instantiate Toolbar.
+		toolbar = findViewById < Toolbar > ( R.id.activity_main_toolbar )
+
+		// Set the Toolbar to ActionBar.
+		setSupportActionBar ( toolbar )
+
+		// Configure the ActionBar.
+		actionBar?.setDisplayHomeAsUpEnabled ( true )
+	}
+
+	/**Method to initialize and configure the DrawerLayout and NavigationView in MainActivity.*/
+	private fun initializeNavigationDrawer () {
+		// Initialize the Drawer Layout.
+		drawerLayout = findViewById < DrawerLayout > ( R.id.activity_main_drawer_layout )
+
+		// Initialize the Navigation View.
+		navigationView = findViewById < NavigationView > ( R.id.activity_main_navigation_view )
+
+		// Set the NavigationItemSelectedListener to NavigationView in order to listen the
+		// selection made by the user in the Navigation Drawer.
+		navigationView?.setNavigationItemSelectedListener ( navigationItemSelectedListener )
 	}
 
 	/*---------------------------- LaunchesFragment.Listener Callbacks ---------------------------*/
