0af73d8ffd75cb94753568b2845896d759ca7cbb
ritwikjamuar <ritwikjamuar@gmail.com>
2019-08-23 19:14:57 +0530

    Created the Dagger Scopes for Application. Thus created following files: 1. ApplicationContext.kt 2. AppScope.kt

---
+++ app/src/main/java/ritwik/samples/spacex/application/di/scopes/AppScope.kt
@@ -0,0 +1,9 @@
+package ritwik.samples.spacex.application.di.scopes
+
+import javax.inject.Scope
+
+/**[Scope] for specifying the privilege of access.
+ * @author Ritwik Jamuar.*/
+@Scope
+@kotlin.annotation.Retention ( AnnotationRetention.RUNTIME )
+annotation class AppScope
