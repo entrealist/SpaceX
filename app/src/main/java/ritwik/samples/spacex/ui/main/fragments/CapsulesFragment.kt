c6a9f2df67f4c48bddd618ad93d7df4e55ff53e4
ritwik.jamuar <ritwik.jamuar@ril.com>
2020-01-08 15:31:22 +0530

    Implemented BaseFragment in CapsulesFragment. Also added code for all the View setup.

--- app/src/main/java/ritwik/samples/spacex/ui/main/fragments/CapsulesFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/main/fragments/CapsulesFragment.kt
@@ -1,26 +1,133 @@
 package ritwik.samples.spacex.ui.main.fragments
 
-import android.os.Bundle
+import android.content.Context
 
-import androidx.fragment.app.Fragment
-
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+
+import androidx.databinding.ViewDataBinding
+
+import androidx.lifecycle.Observer
+
+import androidx.recyclerview.widget.RecyclerView
 
 import ritwik.samples.spacex.R
 
-/**
- * A simple [Fragment] subclass.
- */
-class CapsulesFragment : Fragment() {
+import ritwik.samples.spacex.common.BaseFragment
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_capsules, container, false)
+import ritwik.samples.spacex.component.adapter.CapsuleAdapter
+
+import ritwik.samples.spacex.pojo.capsules.Capsule
+
+import ritwik.samples.spacex.ui.main.mvvm.MainViewModel
+
+import java.lang.RuntimeException
+
+/**[BaseFragment] that shows the list of [Capsule]s.
+ * @author Ritwik Jamuar.*/
+class CapsulesFragment : BaseFragment() {
+
+    // Views.
+    private lateinit var capsules: RecyclerView
+
+    // Adapters.
+    private var adapter: CapsuleAdapter? = null
+
+    // Listeners.
+    private var listener: Listener? = null
+
+    /*------------------------------------- Companion Object -------------------------------------*/
+
+    companion object {
+
+        /**Creates a new instance of [CapsulesFragment].
+         * @return New Instance of [CapsulesFragment].*/
+        @JvmStatic
+        fun create() = CapsulesFragment()
+
+    }
+
+    /*----------------------------------------- Observers ----------------------------------------*/
+
+    /**[Observer] of [List] of [Capsule].*/
+    private val capsulesObserver = Observer<List<Capsule>> {
+        // Set the new List of Capsule in the RecyclerView.Adapter.
+        adapter?.replaceList(it)
+    }
+
+    /*-------------------------------------- View Listeners --------------------------------------*/
+
+    /**Listener Object of [CapsuleAdapter].*/
+    private val adapterListener = object : CapsuleAdapter.Listener {
+        override fun onCapsuleClicked(capsule: Capsule) {
+            // Notify the implementing activity to navigate to fragment that shows Capsule details.
+            listener?.navigateToCapsuleDetail(capsule)
+        }
+    }
+
+    /*---------------------------------- BaseFragment Callbacks ----------------------------------*/
+
+    override fun setListener(context: Context) {
+        if (context is Listener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement Listener")
+        }
+    }
+
+    override fun attachObservers() {
+        listener?.getVM()?.repository?.allCapsulesLiveData?.observe(this, capsulesObserver)
+    }
+
+    override fun layoutRes(): Int = R.layout.fragment_capsules
+
+    override fun isDataBinding(): Boolean = false
+
+    override fun provideDataBinding(binding: ViewDataBinding) = Unit
+
+    override fun initializeViews(view: View) {
+        // Instantiate Adapter.
+        adapter = CapsuleAdapter(adapterListener)
+
+        // Instantiate RecyclerView.
+        capsules = view.findViewById(R.id.fragment_capsules_recycler_view_capsules)
+
+        // Set-up Adapter.
+        capsules.adapter = adapter
+
+        getCapsules()
+    }
+
+    override fun initializeViews() = Unit
+
+    override fun cleanUp() {
+        capsules.adapter = null
+        adapter = null
+    }
+
+    override fun removeListener() {
+        listener = null
+    }
+
+    /*-------------------------------------- Private Methods -------------------------------------*/
+
+    /**Fetches the [List] of [Capsule].*/
+    private fun getCapsules() {
+        listener?.getVM()?.getCapsules()
+    }
+
+    /*---------------------------------------- Interfaces ----------------------------------------*/
+
+    /**Interface Listener for any [android.app.Activity] that uses this [androidx.fragment.app.Fragment].*/
+    interface Listener {
+
+        /**Provides the [androidx.lifecycle.ViewModel] of the attaching [android.app.Activity].
+         * @return [androidx.lifecycle.ViewModel] of [ritwik.samples.spacex.ui.main.MainActivity]*/
+        fun getVM() : MainViewModel
+
+        /**Tells the implementing [android.app.Activity] to navigate to details of [Capsule].
+         * @param capsule Instance of [Capsule] whose details has to be shown.*/
+        fun navigateToCapsuleDetail(capsule: Capsule)
+
     }
 
 }
