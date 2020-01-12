99bebd0955bcd894a3db96a9ff399e2ac325191c
ritwik.jamuar <ritwik.jamuar@ril.com>
2020-01-12 19:39:44 +0530

    Refactored MainRepository by extending to BaseRepository and making use of NetworkProcessor. Due to using NetworkProcessor, RESTServices also got affected.

--- app/src/main/java/ritwik/samples/spacex/application/database/RESTServices.kt
+++ app/src/main/java/ritwik/samples/spacex/application/database/RESTServices.kt
@@ -1,5 +1,6 @@
 package ritwik.samples.spacex.application.database
 
+import retrofit2.Call
 import retrofit2.Response
 
 import retrofit2.http.GET
@@ -13,25 +14,24 @@ import ritwik.samples.spacex.pojo.rockets.Rocket
  * @author Ritwik Jamuar.*/
 interface RESTServices {
 
-	/**Gets the List of Upcoming Launches of SpaceX.
-	 * @return [Response] of type: [List] of [Launch].*/
-	@GET ( ENDPOINT_LAUNCHES + ENDPOINT_SLASH + ENDPOINT_LAUNCHES_TYPE_UPCOMING )
-	suspend fun getUpcomingLaunchesAsync () : Response < List <Launch> >
+    /**Gets the List of Upcoming Launches of SpaceX.
+     * @return [Response] of type: [List] of [Launch].*/
+    @GET(ENDPOINT_LAUNCHES + ENDPOINT_SLASH + ENDPOINT_LAUNCHES_TYPE_UPCOMING)
+    fun getUpcomingLaunches(): Call<List<Launch>>
 
-	/**Gets the List of Past Launches of SpaceX.
-	 * @return [Response] of type: [List] of [Launch].*/
-	@GET ( ENDPOINT_LAUNCHES + ENDPOINT_SLASH + ENDPOINT_LAUNCHES_TYPE_PAST )
-	suspend fun getPastLaunchesAsync () : Response < List <Launch> >
+    /**Gets the List of Past Launches of SpaceX.
+     * @return [Response] of type: [List] of [Launch].*/
+    @GET(ENDPOINT_LAUNCHES + ENDPOINT_SLASH + ENDPOINT_LAUNCHES_TYPE_PAST)
+    fun getPastLaunches(): Call<List<Launch>>
 
-	/**Gets the List of all the Rockets owned by SpaceX.
-	 * @return [Response] of type: [List] of [Rocket].*/
-	@GET ( ENDPOINT_ROCKETS )
-	suspend fun getAllRockets () : Response < List < Rocket > >
+    /**Gets the List of all the Rockets owned by SpaceX.
+     * @return [Response] of type: [List] of [Rocket].*/
+    @GET(ENDPOINT_ROCKETS)
+    fun getAllRockets(): Call<List<Rocket>>
 
-	/**
-	 * Gets the List of all the Capsules owned by SpaceX.
-	 * @return [Response] of type: [List] of [Capsule].*/
-	@GET ( ENDPOINT_CAPSULES )
-	suspend fun getAllCapsules () : Response < List < Capsule > >
+    /**Gets the List of all the Capsules owned by SpaceX.
+     * @return [Response] of type: [List] of [Capsule].*/
+    @GET(ENDPOINT_CAPSULES)
+    fun getAllCapsules(): Call<List<Capsule>>
 
 }
--- app/src/main/java/ritwik/samples/spacex/ui/main/mvvm/MainRepository.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/main/mvvm/MainRepository.kt
@@ -1,14 +1,12 @@
 package ritwik.samples.spacex.ui.main.mvvm
 
-import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
 
-import kotlinx.coroutines.*
-
-import retrofit2.Response
-
-import ritwik.samples.spacex.application.database.LAUNCH_TYPE_PAST
-import ritwik.samples.spacex.application.database.LAUNCH_TYPE_UPCOMING
 import ritwik.samples.spacex.application.database.RESTServices
+
+import ritwik.samples.spacex.common.BaseRepository
+
+import ritwik.samples.spacex.component.other.NetworkProcessor
 
 import ritwik.samples.spacex.pojo.capsules.Capsule
 import ritwik.samples.spacex.pojo.launches.Launch
@@ -16,120 +14,49 @@ import ritwik.samples.spacex.pojo.rockets.Rocket
 
 /**Repository of [MainViewModel].
  * @author Ritwik Jamuar.*/
-class MainRepository (
-	private val restServices : RESTServices?
-) {
-	// LiveData's
-	var upcomingLaunchesLiveData = MutableLiveData < List <Launch> > ()
-	var pastLaunchesLiveData = MutableLiveData < List <Launch> > ()
-	var allRocketsLiveData = MutableLiveData < List < Rocket > > ()
-	var allCapsulesLiveData = MutableLiveData<List<Capsule>>()
+class MainRepository private constructor(
+    restServices: RESTServices
+) : BaseRepository(restServices) {
 
-	// Jobs
-	val completableJob : Job = Job ()
+    /*------------------------------------- Companion Object -------------------------------------*/
 
-	// Lists.
-	private var launches : List <Launch>? = null
-	private var rockets :	List < Rocket >? = null
-	private var capsules: List<Capsule>? = null
+    companion object {
 
-	/*------------------------------------- Companion Object -------------------------------------*/
+        /**Creates the Instance of [MainRepository].
+         * @param restServices Instance of [RESTServices].
+         * @return Instance of [MainRepository]*/
+        fun create(restServices: RESTServices): MainRepository = MainRepository(restServices)
 
-	companion object {
-		/**Static [String] for the future use of Logging.*/
-		@Suppress ( "unused" )
-		const val TAG : String = "MainRepository"
+    }
 
-		/**Creates the Instance of [MainRepository].
-		 * @param restServices Instance of [RESTServices].
-		 * @return Instance of [MainRepository]*/
-		fun create ( restServices : RESTServices? ) : MainRepository {
-			val repository = MainRepository ( restServices )
-			repository.upcomingLaunchesLiveData.value = ArrayList (0 )
-			repository.pastLaunchesLiveData.value = ArrayList ( 0 )
-			return repository
-		}
-	}
+    /*-------------------------------------- Public Methods --------------------------------------*/
 
-	/*-------------------------------------- Public Methods --------------------------------------*/
+    /**Tells the [restServices] to fetch all the Upcoming Launches.
+     * @return [androidx.lifecycle.LiveData] of [NetworkProcessor.Resource] of type [List] of [Launch].*/
+    fun getUpcomingLaunches() = object : NetworkProcessor<List<Launch>, List<Launch>>() {
+        override fun createCall(): Call<List<Launch>> = getRESTServices().getUpcomingLaunches()
+        override fun convertData(initialData: List<Launch>): List<Launch> = initialData
+    }.getData()
 
-	/**Fetches the Launches from [RESTServices] and notify the Observers of
-	 * [upcomingLaunchesLiveData] and [pastLaunchesLiveData], depending on the value passed as
-	 * [type].
-	 * @param type Integer denoting what type of Launch to be fetched. It can be either
-	 * [LAUNCH_TYPE_UPCOMING] or [LAUNCH_TYPE_PAST].*/
-	fun getLaunches ( type : Int ) {
-		CoroutineScope ( Dispatchers.IO + completableJob )
-			.launch {
-				// Get the response on the basis of type.
-				val response : Response < List <Launch> >? = when ( type ) {
-					LAUNCH_TYPE_UPCOMING -> {
-						restServices?.getUpcomingLaunchesAsync ()
-					}
+    /**Tells the [restServices] to fetch all the Past Launches.
+     * @return [androidx.lifecycle.LiveData] of [NetworkProcessor.Resource] of type [List] of [Launch].*/
+    fun getPastLaunches() = object : NetworkProcessor<List<Launch>, List<Launch>>() {
+        override fun createCall(): Call<List<Launch>> = getRESTServices().getPastLaunches()
+        override fun convertData(initialData: List<Launch>): List<Launch> = initialData
+    }.getData()
 
-					LAUNCH_TYPE_PAST -> {
-						restServices?.getPastLaunchesAsync ()
-					}
+    /**Tells the [restServices] to fetch all the Rockets.
+     * @return [androidx.lifecycle.LiveData] of [NetworkProcessor.Resource] of type [List] of [Rocket].*/
+    fun getAllRockets() = object : NetworkProcessor<List<Rocket>, List<Rocket>>() {
+        override fun createCall(): Call<List<Rocket>> = getRESTServices().getAllRockets()
+        override fun convertData(initialData: List<Rocket>): List<Rocket> = initialData
+    }.getData()
 
-					else -> {
-						null
-					}
-				}
-
-				// Notify the observers based on type provided
-				withContext ( Dispatchers.Main ) {
-					// Convert the
-					launches = response?.body ()
-
-					when ( type ) {
-						LAUNCH_TYPE_UPCOMING -> {
-							upcomingLaunchesLiveData.value = launches
-						}
-
-						LAUNCH_TYPE_PAST -> {
-							pastLaunchesLiveData.value = launches
-						}
-					}
-				}
-			}
-	}
-
-	/**Fetches the [List] of [Rocket] from [RESTServices] and notify the
-	 * [androidx.lifecycle.Observer] of [allRocketsLiveData].*/
-	fun getAllRockets () {
-		CoroutineScope ( Dispatchers.IO + completableJob )
-			.launch {
-				// Get the response on the basis of type.
-				val response : Response < List < Rocket > >? = restServices?.getAllRockets ()
-
-				// Convert the Retrofit Response to list of Rockets.
-				rockets = response?.body ()
-
-				// Notify the update of Response in Main Thread.
-				withContext ( Dispatchers.Main ) {
-					// Notify Live Data about change in the List of Rockets.
-					allRocketsLiveData.value = rockets
-				}
-			}
-	}
-
-	/**Fetches the [List] of [Capsule] from [RESTServices] and notify the
-	 * [androidx.lifecycle.Observer] of [allCapsulesLiveData].*/
-	fun getAllCapsules() {
-		CoroutineScope(Dispatchers.IO + completableJob)
-			.launch {
-				// Get the response.
-				val response: Response<List<Capsule>>? = restServices?.getAllCapsules()
-
-				// Convert the Retrofit Response to list of Capsules.
-				capsules = response?.body()
-
-				// Notify the update of Response in Main Thread.
-				withContext(Dispatchers.Main) {
-					// Notify Live Data about change in the List of Capsules.
-					allCapsulesLiveData.value = capsules
-				}
-			}
-	}
+    /**Tells the [restServices] to fetch all the Capsules.
+     * @return [androidx.lifecycle.LiveData] of [NetworkProcessor.Resource] of type [List] of [Capsule].*/
+    fun getAllCapsules() = object : NetworkProcessor<List<Capsule>, List<Capsule>>() {
+        override fun createCall(): Call<List<Capsule>> = getRESTServices().getAllCapsules()
+        override fun convertData(initialData: List<Capsule>): List<Capsule> = initialData
+    }.getData()
 
 }
