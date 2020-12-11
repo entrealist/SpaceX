a5c83985371e571465b737db7f1bee9798635475
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-11 20:45:24 +0530

    Added code in LaunchFragment.kt to store the type of Launch through Bundle Arguments.

--- app/src/main/java/ritwik/samples/spacex/ui/fragment/LaunchFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/fragment/LaunchFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 
 import ritwik.samples.spacex.R
 
+import ritwik.samples.spacex.data.ui.LaunchType
+
 import ritwik.samples.spacex.databinding.FragmentLaunchBinding
 
 import ritwik.samples.spacex.mvvm.model.MainModel
 
 import ritwik.samples.spacex.mvvm.viewModel.MainViewModel
+
+import ritwik.samples.spacex.utility.constant.LAUNCH_TYPE
 
 import sample.ritwik.common.ui.fragment.BaseFragment
 
@@ -19,11 +23,26 @@ import sample.ritwik.common.ui.fragment.BaseFragment
  */
 class LaunchFragment : BaseFragment<FragmentLaunchBinding, MainModel, MainViewModel>() {
 
+    /*------------------------------------- Member Variables -------------------------------------*/
+
+    /**
+     * Reference of [LaunchType] to identify the type of Launch.
+     */
+    private lateinit var launchType: LaunchType
+
     /*---------------------------------- BaseFragment Callbacks ----------------------------------*/
 
     override fun layoutRes(): Int = R.layout.fragment_launch
 
-    override fun extractArguments(arguments: Bundle) = Unit
+    override fun extractArguments(arguments: Bundle) = with(arguments) {
+        if (containsKey(LAUNCH_TYPE)) {
+            launchType = when(getInt(LAUNCH_TYPE)) {
+                0 -> LaunchType.UPCOMING
+                1 -> LaunchType.COMPLETED
+                else -> LaunchType.UPCOMING
+            }
+        }
+    }
 
     override fun initializeViews() = Unit
 
