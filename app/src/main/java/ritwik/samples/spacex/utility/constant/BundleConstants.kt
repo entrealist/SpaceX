5bb1bef7c944fb078e77a1b499ec953923f01555
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-11 20:42:38 +0530

    Added a file BundleConstants.kt with the intent to store Bundle related constants used across the application.

---
+++ app/src/main/java/ritwik/samples/spacex/utility/constant/BundleConstants.kt
@@ -0,0 +1,6 @@
+package ritwik.samples.spacex.utility.constant
+
+/**
+ * Constant [String] denoting the Bundle Parameter for [ritwik.samples.spacex.data.ui.LaunchType].
+ */
+const val LAUNCH_TYPE = "launch_type"
