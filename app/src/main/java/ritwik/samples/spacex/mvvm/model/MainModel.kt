3e237d959cb192f4acbd694290186c8bbb993b94
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-12 01:06:39 +0530

    Added some member variables and methods in MainModel.kt.

--- app/src/main/java/ritwik/samples/spacex/mvvm/model/MainModel.kt
+++ app/src/main/java/ritwik/samples/spacex/mvvm/model/MainModel.kt
@@ -1,6 +1,9 @@
 package ritwik.samples.spacex.mvvm.model
 
+import ritwik.samples.spacex.data.ui.Launch
+
 import sample.ritwik.common.mvvm.model.BaseModel
+
 import javax.inject.Inject
 
 /**
@@ -8,4 +11,38 @@ import javax.inject.Inject
  *
  * @author Ritwik Jamuar
  */
-class MainModel @Inject constructor() : BaseModel()
+class MainModel @Inject constructor() : BaseModel() {
+
+    /*------------------------------------- Member Variables -------------------------------------*/
+
+    /**
+     * [List] of [Launch] denoting the collection of Upcoming Launches.
+     */
+    lateinit var upcomingLaunches: List<Launch>
+
+    /**
+     * [List] of [Launch] denoting the collection of Completed Launches.
+     */
+    lateinit var completedLaunches: List<Launch>
+
+    /*-------------------------------------- Public Methods --------------------------------------*/
+
+    /**
+     * Checks whether [upcomingLaunches] is populated or not,
+     * by checking whether it is initialized as well as it is not empty.
+     *
+     * @return true, if [upcomingLaunches] is initialized and is not empty, else false.
+     */
+    fun isUpcomingLaunchesPopulated(): Boolean =
+        this::upcomingLaunches.isInitialized && upcomingLaunches.isNotEmpty()
+
+    /**
+     * Checks whether [completedLaunches] is populated or not,
+     * by checking whether it is initialized as well as it is not empty.
+     *
+     * @return true, if [completedLaunches] is initialized and is not empty, else false.
+     */
+    fun isCompletedLaunchesPopulated(): Boolean =
+        this::completedLaunches.isInitialized && completedLaunches.isNotEmpty()
+
+}
