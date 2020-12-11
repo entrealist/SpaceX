b01af3b6444c92039e72922c4116c72b8221ab6e
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-11 19:29:03 +0530

    Added Code to set-up the navigation between fragments by adding navigation_graph_main.xml for NavHostFragment, hosted under activity_main.xml.

--- app/src/main/java/ritwik/samples/spacex/ui/activity/MainActivity.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/activity/MainActivity.kt
@@ -4,6 +4,13 @@ import android.view.View
 
 import androidx.lifecycle.ViewModelProvider
 
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+import androidx.navigation.ui.setupWithNavController
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 import com.squareup.picasso.Picasso
 
 import dagger.android.AndroidInjection
@@ -54,6 +61,51 @@ class MainActivity : BaseActivity<MainModel, MainViewModel, ActivityMainBinding>
     @Inject
     lateinit var picassoLibrary: Picasso
 
+    /**
+     * Reference of [NavController] to control the navigation between different fragments.
+     */
+    private lateinit var navigationController: NavController
+
+    /*-------------------------------------- View Listeners --------------------------------------*/
+
+    /**
+     * [NavController.OnDestinationChangedListener] to intercept change in the
+     * destination of [navigationController].
+     */
+    private val destinationChangedListener =
+        NavController.OnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigationView.menu.findItem(
+                when (destination.id) {
+                    R.id.fragment_launch_container -> R.id.menu_item_launches
+                    R.id.fragment_vehicle_container -> R.id.menu_item_vehicles
+                    R.id.fragment_cores -> R.id.menu_item_cores
+                    else -> R.id.fragment_launch_container
+                }
+            ).isChecked = true
+        }
+
+    /**
+     * [BottomNavigationView.OnNavigationItemSelectedListener] to intercept selection of Menu Item.
+     */
+    private val bottomNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+
+            // Perform Action based on Menu Item.
+            navigationController.navigate(
+                when (menuItem.itemId) {
+                    R.id.menu_item_launches -> R.id.fragment_launch_container
+                    R.id.menu_item_vehicles -> R.id.fragment_vehicle_container
+                    R.id.menu_item_cores -> R.id.fragment_cores
+                    else -> 0
+                }
+            )
+
+            // Returning 'true' so that we don't have to perform manual selection of item in the
+            // BottomNavigationView.
+            true
+
+        }
+
     /*---------------------------------- BaseActivity Callbacks ----------------------------------*/
 
     override val viewModel: MainViewModel
@@ -79,7 +131,9 @@ class MainActivity : BaseActivity<MainModel, MainViewModel, ActivityMainBinding>
 
     override fun extractArguments() = Unit
 
-    override fun initialize() = Unit
+    override fun initialize() {
+        setUpViews()
+    }
 
     override fun attachObservers() = Unit
 
@@ -88,5 +142,35 @@ class MainActivity : BaseActivity<MainModel, MainViewModel, ActivityMainBinding>
     override fun onAction(uiData: MainModel) = Unit
 
     override fun cleanUp() = Unit
+
+    /*------------------------------------- Private Methods --------------------------------------*/
+
+    /**
+     * Sets-up all the child views under [binding].
+     */
+    private fun setUpViews() {
+        setUpNavigationView()
+        setUpBottomNavigationView()
+    }
+
+    /**
+     * Sets-up the [navigationController].
+     */
+    private fun setUpNavigationView() {
+        navigationController = Navigation.findNavController(
+            this@MainActivity,
+            R.id.navigation_host_fragment
+        ).apply {
+            addOnDestinationChangedListener(destinationChangedListener)
+        }
+    }
+
+    /**
+     * Sets-up the [BottomNavigationView] under [binding].
+     */
+    private fun setUpBottomNavigationView() = with(binding.bottomNavigationView) {
+        setupWithNavController(navigationController)
+        setOnNavigationItemSelectedListener(bottomNavigationItemSelectedListener)
+    }
 
 }
