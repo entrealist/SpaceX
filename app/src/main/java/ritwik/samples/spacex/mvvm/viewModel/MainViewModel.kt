4b0f0c3568a88b70aefd9ebf2c5589ec12a65ab9
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-12 01:07:24 +0530

    Added a method signature and empty Lambda Expressions.

--- app/src/main/java/ritwik/samples/spacex/mvvm/viewModel/MainViewModel.kt
+++ app/src/main/java/ritwik/samples/spacex/mvvm/viewModel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package ritwik.samples.spacex.mvvm.viewModel
 
+import ritwik.samples.spacex.data.ui.LaunchType
+
 import ritwik.samples.spacex.mvvm.model.MainModel
 
 import ritwik.samples.spacex.mvvm.repository.MainRepository
@@ -27,5 +29,32 @@ class MainViewModel @Inject constructor(
     override fun initializeVariables() = Unit
 
     override fun onSessionExpired() = Unit
+
+    /*-------------------------------------- Public Methods --------------------------------------*/
+
+    /**
+     * Fetches the Launches of given [launchType] from making REST API Calls.
+     *
+     * @param launchType Instance of [LaunchType] denoting the type of Launch.
+     */
+    fun fetchLaunches(launchType: LaunchType) {
+        // TODO: Perform the REST API Call to fetch the Launches of the given launchType.
+    }
+
+    /*------------------------------------ Lambda Expressions ------------------------------------*/
+
+    /**
+     * Lambda Expression to notify about selection of an Upcoming Event.
+     */
+    val upcomingLaunchListener: (Int) -> Unit = { position ->
+        // TODO: Use the position in model.upcomingLaunches to handle the click on Upcoming Launch.
+    }
+
+    /**
+     * Lambda Expression to notify about selection of a Completed Event.
+     */
+    val completedLaunchListener: (Int) -> Unit = { position ->
+        // TODO: Use the position in model.completedLaunches to handle the click on Completed Launch.
+    }
 
 }
