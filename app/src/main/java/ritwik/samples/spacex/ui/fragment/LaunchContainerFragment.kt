65220ec28e387590d8c2c6cc114c2d8b4255a1e2
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-11 20:48:15 +0530

    Added ViewPager and TabLayout in fragment_launch_container.xml, and it's set-up code in LaunchContainerFragment.kt.

--- app/src/main/java/ritwik/samples/spacex/ui/fragment/LaunchContainerFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/fragment/LaunchContainerFragment.kt
@@ -2,6 +2,8 @@ package ritwik.samples.spacex.ui.fragment
 
 import android.os.Bundle
 
+import com.google.android.material.tabs.TabLayoutMediator
+
 import ritwik.samples.spacex.R
 
 import ritwik.samples.spacex.databinding.FragmentLaunchContainerBinding
@@ -9,6 +11,8 @@ import ritwik.samples.spacex.databinding.FragmentLaunchContainerBinding
 import ritwik.samples.spacex.mvvm.model.MainModel
 
 import ritwik.samples.spacex.mvvm.viewModel.MainViewModel
+
+import ritwik.samples.spacex.ui.adapter.LaunchesAdapter
 
 import sample.ritwik.common.ui.fragment.BaseFragment
 
@@ -26,7 +30,10 @@ class LaunchContainerFragment :
 
     override fun extractArguments(arguments: Bundle) = Unit
 
-    override fun initializeViews() = Unit
+    override fun initializeViews() {
+        setUpViewPager()
+        setUpTabLayout()
+    }
 
     override fun showLoading() = Unit
 
@@ -36,6 +43,36 @@ class LaunchContainerFragment :
 
     override fun onAction(uiData: MainModel) = Unit
 
-    override fun cleanUp() = Unit
+    override fun cleanUp() = binding?.let { viewBinding ->
+        viewBinding.fragmentLaunchesViewPager.adapter = null
+    } ?: Unit
+
+    /*------------------------------------- Private Methods --------------------------------------*/
+
+    /**
+     * Sets-up the [androidx.viewpager2.widget.ViewPager2] enclosed under [binding].
+     */
+    private fun setUpViewPager() = binding?.fragmentLaunchesViewPager?.let { viewPager ->
+        viewPager.adapter = LaunchesAdapter(this)
+    } ?: Unit
+
+    /**
+     * Sets-up the [com.google.android.material.tabs.TabLayout] enclosed under [binding].
+     */
+    private fun setUpTabLayout() = binding?.fragmentLaunchesTabLayout?.let { tabLayout ->
+        binding?.fragmentLaunchesViewPager?.let { viewPager ->
+
+            // Set the TabLayoutMediator to set the Tab Text according to the
+            // position of the Fragment currently rendered by the ViewPager.
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = when(position) {
+                    0 -> getString(R.string.launch_type_upcoming)
+                    1 -> getString(R.string.launch_type_completed)
+                    else -> ""
+                }
+            }.attach()
+
+        }
+    } ?: Unit
 
 }
