a7e59858ec0591038a67a2b123510cfd4449a554
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-12 16:34:59 +0530

    Added REST EndPoints in RESTInterface.kt to fetch the Upcoming and Past Launches.

--- app/src/main/java/ritwik/samples/spacex/components/network/RESTInterface.kt
+++ app/src/main/java/ritwik/samples/spacex/components/network/RESTInterface.kt
@@ -1,8 +1,33 @@
 package ritwik.samples.spacex.components.network
 
+import retrofit2.http.GET
+
+import ritwik.samples.spacex.data.network.LaunchResponse
+
+import ritwik.samples.spacex.utility.constant.API_CALL_PAST_LAUNCHES
+import ritwik.samples.spacex.utility.constant.API_CALL_UPCOMING_LAUNCHES
+
 /**
  * Retrofit's Interface to map out different REST End-Points.
  *
  * @author Ritwik Jamuar
  */
-interface RESTInterface
+interface RESTInterface {
+
+    /**
+     * Fetches the Upcoming Launches.
+     *
+     * @return [List] of [LaunchResponse] denoting the Launches.
+     */
+    @GET(API_CALL_UPCOMING_LAUNCHES)
+    suspend fun getUpcomingLaunches(): List<LaunchResponse>
+
+    /**
+     * Fetches the Past Launches.
+     *
+     * @return [List] of [LaunchResponse] denoting the Launches.
+     */
+    @GET(API_CALL_PAST_LAUNCHES)
+    suspend fun getPastLaunches(): List<LaunchResponse>
+
+}
