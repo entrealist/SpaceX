d0141aeccb144361308aa78716609537e1bdc6cc
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-12 16:31:14 +0530

    Added a Kotlin File APIConstants.kt with the intent to store all the constants related to REST API Call.

---
+++ app/src/main/java/ritwik/samples/spacex/utility/constant/APIConstants.kt
@@ -0,0 +1,6 @@
+package ritwik.samples.spacex.utility.constant
+
+/**
+ * Constant [String] denoting the API Call End-Point for REST API to fetch the Upcoming Launches.
+ */
+const val API_CALL_UPCOMING_LAUNCHES = "launches/upcoming"
