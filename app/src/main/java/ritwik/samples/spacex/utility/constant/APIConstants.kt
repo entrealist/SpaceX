7f21c5a4dca552246b25dd99625a8bcb8157151f
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-12 16:34:35 +0530

    Added another constant in APIConstants.kt.

--- app/src/main/java/ritwik/samples/spacex/utility/constant/APIConstants.kt
+++ app/src/main/java/ritwik/samples/spacex/utility/constant/APIConstants.kt
@@ -4,3 +4,8 @@ package ritwik.samples.spacex.utility.constant
  * Constant [String] denoting the API Call End-Point for REST API to fetch the Upcoming Launches.
  */
 const val API_CALL_UPCOMING_LAUNCHES = "launches/upcoming"
+
+/**
+ * Constant [String] denoting the API Call End-Point for REST API to fetch the Past Launches.
+ */
+const val API_CALL_PAST_LAUNCHES = "launches/past"
