d7840c314e0e8cbb28852e24021133272b4874fd
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-13 17:39:35 +0530

    Added code to show RocketsFragment.kt and CapsulesFragment.kt under a ViewPager2 in VehicleContainerFragment.kt. Thus also affected fragment_vehicle_container.xml.

--- app/src/main/java/ritwik/samples/spacex/ui/fragment/VehicleContainerFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/fragment/VehicleContainerFragment.kt
@@ -2,6 +2,8 @@ package ritwik.samples.spacex.ui.fragment
 
 import android.os.Bundle
 
+import com.google.android.material.tabs.TabLayoutMediator
+
 import ritwik.samples.spacex.R
 
 import ritwik.samples.spacex.databinding.FragmentVehicleContainerBinding
@@ -9,6 +11,8 @@ import ritwik.samples.spacex.databinding.FragmentVehicleContainerBinding
 import ritwik.samples.spacex.mvvm.model.MainModel
 
 import ritwik.samples.spacex.mvvm.viewModel.MainViewModel
+
+import ritwik.samples.spacex.ui.adapter.VehiclesAdapter
 
 import sample.ritwik.common.ui.fragment.BaseFragment
 
@@ -26,7 +30,10 @@ class VehicleContainerFragment :
 
     override fun extractArguments(arguments: Bundle) = Unit
 
-    override fun initializeViews() = Unit
+    override fun initializeViews() {
+        setUpViewPager()
+        setUpTabLayout()
+    }
 
     override fun showLoading() = Unit
 
@@ -36,6 +43,36 @@ class VehicleContainerFragment :
 
     override fun onAction(uiData: MainModel) = Unit
 
-    override fun cleanUp() = Unit
+    override fun cleanUp() = binding?.let { viewBinding ->
+        viewBinding.fragmentVehiclesViewPager.adapter = null
+    } ?: Unit
+
+    /*------------------------------------- Private Methods --------------------------------------*/
+
+    /**
+     * Sets-up the [androidx.viewpager2.widget.ViewPager2] enclosed under [binding].
+     */
+    private fun setUpViewPager() = binding?.fragmentVehiclesViewPager?.let { viewPager ->
+        viewPager.adapter = VehiclesAdapter(this)
+    } ?: Unit
+
+    /**
+     * Sets-up the [com.google.android.material.tabs.TabLayout] enclosed under [binding].
+     */
+    private fun setUpTabLayout() = binding?.fragmentVehiclesTabLayout?.let { tabLayout ->
+        binding?.fragmentVehiclesViewPager?.let { viewPager ->
+
+            // Set the TabLayoutMediator to set the Tab Text according to the
+            // position of the Fragment currently rendered by the ViewPager.
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = when(position) {
+                    0 -> getString(R.string.vehicle_type_rocket)
+                    1 -> getString(R.string.vehicle_type_capsule)
+                    else -> ""
+                }
+            }.attach()
+
+        }
+    } ?: Unit
 
 }
