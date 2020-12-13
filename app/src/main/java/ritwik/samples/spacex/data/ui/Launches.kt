92ea587676fbdf9b56284c5ac8a5a6df3ee456ad
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-13 22:46:38 +0530

    Moved the classes Launches.kt and LaunchType.kt under Launches.kt.

--- app/src/main/java/ritwik/samples/spacex/data/ui/Launches.kt
+++ app/src/main/java/ritwik/samples/spacex/data/ui/Launches.kt
@@ -35,3 +35,23 @@ data class Launch(
     fun getBlockNumberText(): String = "$blockNumber"
 
 }
+
+
+/**
+ * Enumeration to classify different kinds of Launches of SpaceX.
+ *
+ * @author Ritwik Jamuar
+ */
+enum class LaunchType {
+
+    /**
+     * Denotes the Upcoming Launches.
+     */
+    UPCOMING,
+
+    /**
+     * Denotes the Completed Launches.
+     */
+    COMPLETED
+
+}
