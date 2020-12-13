4870d9041d2a8b580ac0d564f1ba6ca54ec0aa7c
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-13 15:28:27 +0530

    Added code to show the Count-Down timer for Upcoming Launches in LaunchFragment.kt.

--- app/src/main/java/ritwik/samples/spacex/ui/fragment/LaunchFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/fragment/LaunchFragment.kt
@@ -1,6 +1,7 @@
 package ritwik.samples.spacex.ui.fragment
 
 import android.os.Bundle
+import android.os.CountDownTimer
 
 import android.view.View
 
@@ -22,6 +23,8 @@ import ritwik.samples.spacex.utility.constant.POPULATE_LAUNCHES
 
 import sample.ritwik.common.ui.fragment.BaseFragment
 
+import java.util.concurrent.TimeUnit
+
 /**
  * [BaseFragment] to show the list of Launches of given type.
  *
@@ -35,6 +38,11 @@ class LaunchFragment : BaseFragment<FragmentLaunchBinding, MainModel, MainViewMo
      * Reference of [LaunchType] to identify the type of Launch.
      */
     private lateinit var launchType: LaunchType
+
+    /**
+     * Reference of [CountDownTimer] to start/cancel the Count-Down.
+     */
+    private var countDownTimer: CountDownTimer? = null
 
     /*---------------------------------- BaseFragment Callbacks ----------------------------------*/
 
@@ -79,6 +87,7 @@ class LaunchFragment : BaseFragment<FragmentLaunchBinding, MainModel, MainViewMo
                         (listLaunches.adapter as? UpcomingLaunchAdapter)?.replaceList(
                             uiData.upcomingLaunches
                         ) ?: Unit
+                        handleCountDownTimer(uiData.calculateTimingForUpcomingLaunch())
                     }
 
                     LaunchType.COMPLETED -> {
@@ -100,6 +109,11 @@ class LaunchFragment : BaseFragment<FragmentLaunchBinding, MainModel, MainViewMo
     override fun cleanUp() = binding?.let { dataBinding ->
         dataBinding.listLaunches.adapter = null
     } ?: Unit
+
+    override fun onDetach() {
+        super.onDetach()
+        cleanUpCountDownTimer()
+    }
 
     /*------------------------------------- Private Methods --------------------------------------*/
 
@@ -123,5 +137,91 @@ class LaunchFragment : BaseFragment<FragmentLaunchBinding, MainModel, MainViewMo
      * Requests the data through [viewModel].
      */
     private fun requestData() = viewModel?.fetchLaunches(launchType) ?: Unit
+
+    /**
+     * Provides the instance of [CountDownTimer].
+     *
+     * @param duration [Long] denoting the duration till which the [CountDownTimer] would run.
+     * @return New Instance of [CountDownTimer].
+     */
+    private fun provideCountDownTimer(duration: Long) = object: CountDownTimer(duration, 1000) {
+
+        override fun onTick(milliSecondsUntilFinished: Long) {
+            binding?.valueTextLatestMissionRemainingTime?.let { timeView ->
+                timeView.post {
+                    timeView.text = provideFormattedTime(milliSecondsUntilFinished)
+                }
+            }
+        }
+
+        override fun onFinish() {
+            binding?.valueTextLatestMissionRemainingTime?.visibility = View.GONE
+        }
+
+    }
+
+    /**
+     * Provides the given [time] in a Formatted [String] with the format below:
+     *
+     *
+     * Day : Hour : Minute : Second
+     *
+     * @param time [Long] denoting the Time to be displayed in the format.
+     * @return [String] denoting the given [time] in the Time Format.
+     */
+    private fun provideFormattedTime(time: Long): String {
+
+        // Create a reference to store the current 'time'.
+        var uptime = time
+
+        // Get the no. of days from the given 'time'
+        // and then subtract the 'day' equivalent Milli-Seconds from the 'time'.
+        val day = TimeUnit.MILLISECONDS.toDays(uptime)
+        uptime -= TimeUnit.DAYS.toMillis(day)
+
+        // Get the no. of hours from the given 'time'
+        // and then subtract the 'hour' equivalent Milli-Seconds from the 'time'.
+        val hour = TimeUnit.MILLISECONDS.toHours(uptime)
+        uptime -= TimeUnit.HOURS.toMillis(hour)
+
+        // Get the no. of minutes from the given 'time'
+        // and then subtract the 'minute' equivalent Milli-Seconds from the 'time'.
+        val minute = TimeUnit.MILLISECONDS.toMinutes(uptime)
+        uptime -= TimeUnit.MINUTES.toMillis(minute)
+
+        // Get the no. of seconds from the given 'time'.
+        val second = TimeUnit.MILLISECONDS.toSeconds(uptime)
+
+        // Return the Formatted Time as String.
+        return "$day : $hour : $hour : $second"
+
+    }
+
+    /**
+     * Takes care of handling the [countDownTimer].
+     *
+     * @param countDownTime [Long] denoting the Count-Down Time from which the Timer would start.
+     */
+    private fun handleCountDownTimer(countDownTime: Long) {
+
+        // Halt the further execution and clean the if the 'countDownTime' is 0.
+        if (countDownTime == 0L) return
+
+        // Clean-Up the previously instantiated Count-Down Timer.
+        cleanUpCountDownTimer()
+
+        // Initialize and Start the Count-Down Timer.
+        countDownTimer = provideCountDownTimer(countDownTime)
+        countDownTimer?.start()
+
+    }
+
+    /**
+     * Clean-up the [countDownTimer] to avoid the Memory Leak.
+     */
+    private fun cleanUpCountDownTimer() {
+        countDownTimer?.cancel() // Cancel the Count-Down Timer first.
+        countDownTimer = null // De-reference the instance.
+    }
 
 }
