4f9bb15f25b1259d1b9db4eda8acf02f3f8e11e0
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-14 16:22:04 +0530

    Added code in CapsulesFragment.kt to render the List of Capsules.

--- app/src/main/java/ritwik/samples/spacex/ui/fragment/CapsulesFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/fragment/CapsulesFragment.kt
@@ -2,6 +2,8 @@ package ritwik.samples.spacex.ui.fragment
 
 import android.os.Bundle
 
+import android.view.View
+
 import ritwik.samples.spacex.R
 
 import ritwik.samples.spacex.databinding.FragmentCapsulesBinding
@@ -9,6 +11,10 @@ import ritwik.samples.spacex.databinding.FragmentCapsulesBinding
 import ritwik.samples.spacex.mvvm.model.MainModel
 
 import ritwik.samples.spacex.mvvm.viewModel.MainViewModel
+
+import ritwik.samples.spacex.ui.adapter.CapsulesAdapter
+
+import ritwik.samples.spacex.utility.constant.POPULATE_CAPSULES
 
 import sample.ritwik.common.ui.fragment.BaseFragment
 
@@ -25,16 +31,55 @@ class CapsulesFragment : BaseFragment<FragmentCapsulesBinding, MainModel, MainVi
 
     override fun extractArguments(arguments: Bundle) = Unit
 
-    override fun initializeViews() = Unit
+    override fun initializeViews() {
+        setUpView()
+        requestData()
+    }
 
-    override fun showLoading() = Unit
+    override fun showLoading() = binding?.placeholderShimmerContainer?.let { shimmer ->
+        shimmer.visibility = View.VISIBLE
+        shimmer.startShimmerAnimation()
+    } ?: Unit
 
-    override fun hideLoading() = Unit
+    override fun hideLoading() = binding?.placeholderShimmerContainer?.let { shimmer ->
+        shimmer.stopShimmerAnimation()
+        shimmer.visibility = View.GONE
+    } ?: Unit
 
     override fun onUIDataChanged(uiData: MainModel) = Unit
 
-    override fun onAction(uiData: MainModel) = Unit
+    override fun onAction(uiData: MainModel) = when(uiData.action) {
 
-    override fun cleanUp() = Unit
+        POPULATE_CAPSULES -> {
+            binding?.listCapsules?.adapter?.let { adapter ->
+                (adapter as? CapsulesAdapter)?.replaceList(uiData.capsules) ?: Unit
+            } ?: Unit
+        }
+
+        else -> Unit
+
+    }
+
+    override fun cleanUp() = binding?.let { dataBinding ->
+        dataBinding.listCapsules.adapter = null
+    } ?: Unit
+
+    /*------------------------------------- Private Methods --------------------------------------*/
+
+    /**
+     * Sets-up the views under [binding].
+     */
+    private fun setUpView() = binding?.let { dataBinding ->
+        viewModel?.let { vm ->
+            with(dataBinding) {
+                listCapsules.adapter = CapsulesAdapter(vm.rocketListener)
+            }
+        } ?: Unit
+    } ?: Unit
+
+    /**
+     * Requests the data through [viewModel].
+     */
+    private fun requestData() = viewModel?.fetchCapsules() ?: Unit
 
 }
