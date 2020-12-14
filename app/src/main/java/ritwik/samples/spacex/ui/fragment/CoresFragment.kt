6d22467cf3fda956ff122017a9aae2b4eb1a4abc
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-14 20:56:53 +0530

    Added code to render the collection of Cores fetched from REST API Call.

--- app/src/main/java/ritwik/samples/spacex/ui/fragment/CoresFragment.kt
+++ app/src/main/java/ritwik/samples/spacex/ui/fragment/CoresFragment.kt
@@ -2,6 +2,8 @@ package ritwik.samples.spacex.ui.fragment
 
 import android.os.Bundle
 
+import android.view.View
+
 import ritwik.samples.spacex.R
 
 import ritwik.samples.spacex.databinding.FragmentCoresBinding
@@ -9,6 +11,10 @@ import ritwik.samples.spacex.databinding.FragmentCoresBinding
 import ritwik.samples.spacex.mvvm.model.MainModel
 
 import ritwik.samples.spacex.mvvm.viewModel.MainViewModel
+
+import ritwik.samples.spacex.ui.adapter.CoresAdapter
+
+import ritwik.samples.spacex.utility.constant.POPULATE_CORES
 
 import sample.ritwik.common.ui.fragment.BaseFragment
 
@@ -25,16 +31,55 @@ class CoresFragment : BaseFragment<FragmentCoresBinding, MainModel, MainViewMode
 
     override fun extractArguments(arguments: Bundle) = Unit
 
-    override fun initializeViews() = Unit
+    override fun initializeViews() {
+        setUpView()
+        requestData()
+    }
 
-    override fun showLoading() = Unit
+    override fun showLoading() = binding?.placeholderShimmerContainer?.let { shimmer ->
+        shimmer.visibility = View.VISIBLE
+        shimmer.startShimmerAnimation()
+    } ?: Unit
 
-    override fun hideLoading() = Unit
+    override fun hideLoading() = binding?.placeholderShimmerContainer?.let { shimmer ->
+        shimmer.stopShimmerAnimation()
+        shimmer.visibility = View.GONE
+    } ?: Unit
 
     override fun onUIDataChanged(uiData: MainModel) = Unit
 
-    override fun onAction(uiData: MainModel) = Unit
+    override fun onAction(uiData: MainModel) = when(uiData.action) {
 
-    override fun cleanUp() = Unit
+        POPULATE_CORES -> {
+            binding?.listCores?.adapter?.let { adapter ->
+                (adapter as? CoresAdapter)?.replaceList(uiData.cores) ?: Unit
+            } ?: Unit
+        }
+
+        else -> Unit
+
+    }
+
+    override fun cleanUp() = binding?.let { dataBinding ->
+        dataBinding.listCores.adapter = null
+    } ?: Unit
+
+    /*------------------------------------- Private Methods --------------------------------------*/
+
+    /**
+     * Sets-up the views under [binding].
+     */
+    private fun setUpView() = binding?.let { dataBinding ->
+        viewModel?.let { vm ->
+            with(dataBinding) {
+                listCores.adapter = CoresAdapter(vm.coreListener)
+            }
+        } ?: Unit
+    } ?: Unit
+
+    /**
+     * Requests the data through [viewModel].
+     */
+    private fun requestData() = viewModel?.fetchCores() ?: Unit
 
 }
