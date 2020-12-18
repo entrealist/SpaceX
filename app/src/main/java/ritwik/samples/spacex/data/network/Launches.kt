7c4b8eda3fb7704171e299c7888a32acbfdb3e46
ritwikjamuar <ritwikjamuar@gmail.com>
2020-12-18 23:03:12 +0530

    Changed the names of the class under data -> Network -> Launches.kt.

--- app/src/main/java/ritwik/samples/spacex/data/network/Launches.kt
+++ app/src/main/java/ritwik/samples/spacex/data/network/Launches.kt
@@ -3,8 +3,8 @@ package ritwik.samples.spacex.data.network
 import com.squareup.moshi.Json
 
 data class LaunchResponse(
-    @field: Json(name = "fairings") val fairings: LaunchFairings?,
-    @field: Json(name = "links") val links: LaunchLinks?,
+    @field: Json(name = "fairings") val fairings: LaunchFairingResponse?,
+    @field: Json(name = "links") val links: LaunchLinksResponse?,
     @field: Json(name = "static_fire_date_utc") val utcStaticFireDate: String?,
     @field: Json(name = "static_fire_date_unix") val unixStaticFireDate: Long?,
     @field: Json(name = "tbd") val tbd: Boolean?,
@@ -19,7 +19,7 @@ data class LaunchResponse(
     @field: Json(name = "payloads") val payloads: List<String>?,
     @field: Json(name = "launchpad") val launchPad: String?,
     @field: Json(name = "auto_update") val autoUpdate: Boolean?,
-    @field: Json(name = "failures") val failures: List<LaunchFailure>?,
+    @field: Json(name = "failures") val failures: List<LaunchFailureResponse>?,
     @field: Json(name = "flight_number") val flightNumber: Int?,
     @field: Json(name = "name") val name: String?,
     @field: Json(name = "date_utc") val utcDate: String?,
@@ -27,11 +27,11 @@ data class LaunchResponse(
     @field: Json(name = "date_local") val localDate: String?,
     @field: Json(name = "date_precision") val datePrecision: String?,
     @field: Json(name = "upcoming") val upcoming: Boolean?,
-    @field: Json(name = "cores") val cores: List<LaunchCore>,
+    @field: Json(name = "cores") val cores: List<LaunchCoreResponse>,
     @field: Json(name = "id") val id: String?
 )
 
-data class LaunchCore(
+data class LaunchCoreResponse(
     @field: Json(name = "core") val core: String?,
     @field: Json(name = "flight") val flight: Int?,
     @field: Json(name = "gridfins") val gridFins: Boolean?,
@@ -43,17 +43,17 @@ data class LaunchCore(
     @field: Json(name = "landing_vehicle") val landingVehicle: String?
 )
 
-data class LaunchFairings(
+data class LaunchFairingResponse(
     @field: Json(name = "reused") val reused: Boolean?,
     @field: Json(name = "recovery_attempt") val recoveryAttempt: Boolean?,
     @field: Json(name = "recovered") val recovered: Boolean?,
     @field: Json(name = "ship") val ship: List<String>?
 )
 
-data class LaunchLinks(
-    @field: Json(name = "patch") val patch: LaunchLinkPatch?,
-    @field: Json(name = "reddit") val reddit: LaunchLinkReddit?,
-    @field: Json(name = "flickr") val flickr: LaunchLinkFlickr?,
+data class LaunchLinksResponse(
+    @field: Json(name = "patch") val patch: LaunchLinkPatchResponse?,
+    @field: Json(name = "reddit") val reddit: LaunchLinkRedditResponse?,
+    @field: Json(name = "flickr") val flickr: LaunchLinkFlickrResponse?,
     @field: Json(name = "presskit") val pressKit: String?,
     @field: Json(name = "webcast") val webCast: String?,
     @field: Json(name = "youtube_id") val youtubeID: String?,
@@ -61,24 +61,24 @@ data class LaunchLinks(
     @field: Json(name = "wikipedia") val wikipedia: String?
 )
 
-data class LaunchLinkPatch(
+data class LaunchLinkPatchResponse(
     @field: Json(name = "small") val small: String?,
     @field: Json(name = "large") val large: String?
 )
 
-data class LaunchLinkReddit(
+data class LaunchLinkRedditResponse(
     @field: Json(name = "campaign") val campaign: String?,
     @field: Json(name = "launch") val launch: String?,
     @field: Json(name = "media") val media: String?,
     @field: Json(name = "recovery") val recovery: String?
 )
 
-data class LaunchLinkFlickr(
+data class LaunchLinkFlickrResponse(
     @field: Json(name = "small") val small: List<String>?,
     @field: Json(name = "original") val original: List<String>?
 )
 
-data class LaunchFailure(
+data class LaunchFailureResponse(
     @field: Json(name = "time") val time: Int?,
     @field: Json(name = "altitude") val altitude: Int?,
     @field: Json(name = "reason") val reason: String?
